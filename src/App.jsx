62a7607a6b1104dbb76516dcaf1ded193f540c84
Amannroy <royaman56456@gamil.com>
2024-01-26 17:43:21 +0530

    Configured auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Routes>
       lms
-    </>
+    </Routes>
   )
 }
 
